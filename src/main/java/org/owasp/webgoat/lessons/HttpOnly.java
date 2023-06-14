3df18a483e29d1e37cbb5c6e93453437fd7ecdef
mayyuser <135953836+mayyuser@users.noreply.github.com>
2023-06-14 12:46:31 +0400

    k

--- src/main/java/org/owasp/webgoat/lessons/HttpOnly.java
+++ src/main/java/org/owasp/webgoat/lessons/HttpOnly.java
@@ -183,7 +183,7 @@ public class HttpOnly extends LessonAdapter
         String value = null;
         byte[] buffer = null;
         MessageDigest md = null;
-        BASE64Encoder encoder = new BASE64Encoder();
+        BASE64 encoder = new BASE64();
 
         try
         {
